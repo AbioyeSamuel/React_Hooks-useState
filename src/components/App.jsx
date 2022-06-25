01f931aeddab399e6d4ad9bb5d4ac006dbbc5baa
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-25 20:13:26 +0100

    Created a dercrease button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,21 @@ import React, { useState } from "react";
 
 function App() {
   // React Hooks - useState
-  const [count, setCount] = useState(123);
+  const [count, setCount] = useState(0);
 
   console.log(count);
 
   function increase() {
-    setCount(34);
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
   }
   return (
     <div className="container">
       <h1>{count}</h1>
       <button onClick={increase}>+</button>
+      <button onClick={decrease}>+</button>
     </div>
   );
 }
