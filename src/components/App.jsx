4056599bff47ae9dcdcef35d347c940c9904960e
Samuel Abioye <101730798+AbioyeSamuel@users.noreply.github.com>
2022-06-25 20:11:23 +0100

    Applying Destructuring to our Count function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@ import React, { useState } from "react";
 
 function App() {
   // React Hooks - useState
-  const [red, green, blue] = useState(123, 34, 56);
-  console.log(green);
+  const [count, setCount] = useState(123);
 
-  function increase() {}
+  console.log(count);
+
+  function increase() {
+    setCount(34);
+  }
   return (
     <div className="container">
-      <h1>{state}</h1>
+      <h1>{count}</h1>
       <button onClick={increase}>+</button>
     </div>
   );
